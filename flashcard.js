8e450f7ad129aabb252a4a1f54b5087e4f0ebabb
Septian A Tama <tama@tamatamvan.web.id>
2016-10-07 11:20:27 +0700

    put back flaschcard.js

---
+++ flashcard.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+class Flashcard {
+  static getData(file) {
+    let deck = fs.readFileSync('deck.json');
+    return JSON.parse(deck);
+  }
+}
+
+class FlashcardController {
+  constructor(args = {}) {
+    this.point = [];
+    this.answer = ""
+    this.current_soal = 0
+  }
+
+  generate_soal() {
+    var soalsoal = Flashcard.getData()
+    var soal = soalsoal[this.current_soal]['definition']
+    this.current_soal += 1
+    return soal
+  }
+
+  cek_answer() {
+    if (answer === data[i]['term']) {
+      this.point.push(4)
+    } else {
+      this.point.push(-1)
+    }
+  }
+
+  scoring() {
+    var result = 0
+    for (var i = 0; i < this.point.length; i++) {
+      x = x + this.point[i]
+    }
+    return result
+  }
+
+}
+
+class FlashcardView {
+  constructor(args = {}) {
+    this.varr = args['varr'];
+  }
+}
+
+let controller = new FlashcardController();
+let view = new FlashcardView();
+console.log(controller.generate_soal())
