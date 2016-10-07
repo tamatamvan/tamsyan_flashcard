7164a305505dc5e82461796b0039472850502e04
Septian A Tama <tama@tamatamvan.web.id>
2016-10-07 10:15:18 +0700

    MVC skeleton

---
+++ flashcard.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+class Flashcard {
+  static getData() {
+
+  }
+}
+
+class FlashcardController {
+  constructor(args={}) {
+
+  }
+}
+
+class FlashcardView {
+  constructor(args={}) {
+    this.varr = args['varr'];
+  }
+}
+
+let controller = new FlashcardController();
+let view = new FlashcardView();
